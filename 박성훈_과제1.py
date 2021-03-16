14b9529a548062c00e8a5b9dd666859c841d98fd
Hooooooon <shonn2323@gmail.com>
2021-03-16 17:21:55 +0900

    파이썬활용 과제1

---
+++ 박성훈_과제1.py
@@ -0,0 +1,53 @@
+import turtle
+
+t = turtle.Turtle()
+
+# 별
+t.forward(50)
+t.left(72)
+t.forward(50)
+t.right(72*2)
+t.forward(50)
+
+t.left(72)
+t.forward(50)
+t.right(72*2)
+t.forward(50)
+
+t.left(72)
+t.forward(50)
+t.right(72*2)
+t.forward(50)
+
+t.left(72)
+t.forward(50)
+t.right(72*2)
+t.forward(50)
+
+t.left(72)
+t.forward(50)
+t.right(72*2)
+
+
+# 모래시계
+t.penup()
+t.goto(65, -170)
+t.pendown()
+
+t.left(60)
+t.forward(50)
+
+t.left(120)
+t.forward(50)
+
+t.left(120)
+t.forward(100)
+
+t.right(120)
+t.forward(50)
+
+t.right(120)
+t.forward(50)
+
+t.hideturtle()
+
